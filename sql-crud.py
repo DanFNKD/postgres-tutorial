1e6e65b92994aeb723673e533e53a96047212919
Daniel Harris <dwh261@gmail.com>
2024-09-15 07:57:09 +0000

    Added myself to the database

--- sql-crud.py
+++ sql-crud.py
@@ -79,13 +79,22 @@ tim_berners_lee = Programmer(
     famous_for="World Wide Web"
 )
 
+daniel_harris = Programmer(
+    first_name="Daniel",
+    last_name="Harris",
+    gender="M",
+    nationality="British",
+    famous_for="Beginner Coder"
+)
+
 # add each instance of our programmers to our session
 # session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margaret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+session.add(daniel_harris)
 
 # commit our session to the database
 session.commit()
